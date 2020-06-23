cc8e55230ec430c83433a2fd2f32b6b3541dc5ca
Yuhei Yamamoto <>
2020-06-23 16:37:13 +0900

    リクエスト前後の処理を共通化するInterceptorの例

--- app/src/main/java/org/yy/android_hilt_retrofit/MainActivity.kt
+++ app/src/main/java/org/yy/android_hilt_retrofit/MainActivity.kt
@@ -12,20 +12,57 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.components.ActivityComponent
 import kotlinx.coroutines.launch
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.internal.http2.Http2Reader.Companion.logger
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import java.io.IOException
 import javax.inject.Inject
 
+
+internal class LoggingInterceptor : Interceptor {
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val t1 = System.nanoTime()
+        logger.info(
+            java.lang.String.format(
+                "Sending request %s on %s%n%s",
+                request.url, chain.connection(), request.headers
+            )
+        )
+        val response: Response = chain.proceed(request)
+        val t2 = System.nanoTime()
+        logger.info(
+            java.lang.String.format(
+                "Received response for %s in %.1fms%n%s",
+                response.request.url, (t2 - t1) / 1e6, response.headers
+            )
+        )
+        return response
+    }
+}
 
 @Module
 @InstallIn(ActivityComponent::class)
 object HealthCheckModule {
 
     @Provides
-    fun provideRetrofit():Retrofit{
+    fun provideOkHttpClient():OkHttpClient{
+        return OkHttpClient()
+            .newBuilder()
+            .addInterceptor(LoggingInterceptor())
+            .build()
+    }
+
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient):Retrofit{
         return Retrofit.Builder()
                 .baseUrl("https://api.github.com/")
+                .client(okHttpClient)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
     }
